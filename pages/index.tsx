7dd2472d7fb80c0dcf487b7e7793597f0282537c
Bug <jungmokang99@gmail.com>
2023-05-26 19:48:44 -0400

    Added Rows, thumbnails and scroll function

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import Banner from "@/components/Banner";
 import requests from "@/utils/requests";
 import { Movie } from "@/typings";
+import Row from "@/components/Row";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -28,19 +29,20 @@ export default function Home({
 }: Props) {
   return (
     <div
-      className="relative h-screen bg-gradient-to-b from-gray-900/10
-   to-[#010511] lg:h-[140vh]"
+      className="relative h-screen bg-gradient-to-b lg:h-[140vh]"
     >
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
-        <section>
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
+        <section className="md:space-y-24">
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List Component */}
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
       {/* Modal */}
@@ -67,11 +69,11 @@ export const getServerSideProps = async () => {
     fetch(requests.fetchHorrorMovies).then((res) => res.json()),
     fetch(requests.fetchRomanceMovies).then((res) => res.json()),
     fetch(requests.fetchDocumentaries).then((res) => res.json()),
-  ])
+  ]);
 
   return {
     props: {
-      netflixOriginals: netflixOriginals.results, 
+      netflixOriginals: netflixOriginals.results,
       trendingNow: trendingNow.results,
       topRated: topRated.results,
       actionMovies: actionMovies.results,
@@ -80,6 +82,6 @@ export const getServerSideProps = async () => {
       romanceMovies: romanceMovies.results,
       documentaries: documentaries.results,
       // products,
-    }
-  }
-}
+    },
+  };
+};
